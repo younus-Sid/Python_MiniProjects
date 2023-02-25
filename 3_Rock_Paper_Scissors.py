584b4c1bd67f0749cc14a40298d6041b8d6420ee
Younus Siddique <91816645+younus-Sid@users.noreply.github.com>
2023-02-25 12:13:53 +0530

    Rock Paper Scissors Game

--- 3_Rock_Paper_Scissors.py
+++ 3_Rock_Paper_Scissors.py
@@ -2,18 +2,73 @@
 In this game the player chooses between Rock, Paper, Scissors against the computer.
 """
 
+# Code For Rock Paper Scissors
+
+# Sign Of 'Rock', 'Paper', 'Scissors'
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)         ROCK {}
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)    PAPER {}
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)    SCISSORS {}
+      (____)
+---.__(___)
+'''
+
+
 import random
-def play():
-    user_input = input("Enter 'r' for rock, 'p' for paper, 's' for scissors: ")
-    computer_guess = random.choice(['r','p','s'])
-    if user_input == computer_guess:
-    	print("It's a tie !!")
-    	return play()
-    if is_win(user_input,computer_guess):
-    	return "Woohoo!! You Won."
-    return "Sorry!! You lost"
-    
-def is_win(player1, player2):
-	if (player1 == 'r' and player2 == 's') or (player1 == 's' and player2 == 'p') or (player1 == 'p' and player2 == 'r'):
-		return True
-print(play())	
+
+print("Welcome! To Rock Paper Scissors Game")
+player_choice = int(input("What do you choose? [ 0 For 'Rock', 1 For 'Paper', 2 For 'Scissors' ]\n"))
+computer_choice = random.randint(0, 2)
+if player_choice in range(0, 3):
+    x = "[ YOU ]"
+    sign_list = [rock.format(x), paper.format(x), scissors.format(x)]
+    print(sign_list[player_choice])
+
+    x = "[ COMPUTER ]"
+    sign_list = [rock.format(x), paper.format(x), scissors.format(x)]
+    print(sign_list[computer_choice] + "\n")
+
+    if player_choice == 0: # 'Rock'
+        if computer_choice == 0:   # 'Rock'
+            print("IT'S A DRAW.")
+        elif computer_choice == 1: # 'Paper'
+            print("YOU LOSE!")
+        elif computer_choice == 2: # 'Scissors'
+            print("YOU WIN!")
+    elif player_choice == 1: # 'Paper'
+        if computer_choice == 1:   # 'Paper'
+            print("IT'S A DRAW.")
+        elif computer_choice == 2: # 'Scissors'
+            print("YOU LOSE!")
+        elif computer_choice == 0: # 'Rock'
+            print("YOU WIN!")
+    elif player_choice == 2: # 'Scissors'
+        if computer_choice == 2:   # 'Scissors'
+            print("IT'S A DRAW.")
+        elif computer_choice == 0: # 'Rock'
+            print("YOU LOSE!")
+        elif computer_choice == 1: # 'Paper'
+            print("YOU WIN!")
+else:
+    print("Invalid choice! You Lose.")
+
+print("")
