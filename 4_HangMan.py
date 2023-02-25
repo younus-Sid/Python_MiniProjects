cddea0730af759b364a8465b34fdb26567d3eb63
Younus Siddique <91816645+younus-Sid@users.noreply.github.com>
2023-02-25 12:12:44 +0530

    Hangman Game

--- 4_HangMan.py
+++ 4_HangMan.py
@@ -1,2 +1,99 @@
 
-# Code will be available soon 🤗
+# Code For Hangman Game
+
+print("Welcome! To Hangman Game")
+import random, string
+
+words = ['abruptly', 'absurd', 'affix', 'avenue', 'awkward', 'axiom', 'azure', 'bandwagon', 'banjo', 'beekeeper', 'bikini', 'blitz', 'blizzard', 'boggle', 'bookworm', 'boxcar', 'boxful', 'buckaroo', 'buffalo', 'buffoon', 'buxom', 'buzzard', 'buzzing', 'buzzwords', 'cobweb', 'cockiness', 'croquet', 'crypt', 'curacao', 'cycle', 'daiquiri', 'dirndl', 'disavow', 'dizzying', 'duplex', 'dwarves', 'embezzle', 'equip', 'espionage', 'euouae', 'exodus', 'faking', 'fishhook', 'fixable', 'fjord', 'flapjack', 'flopping', 'fluffiness', 'flyby', 'foxglove', 'frazzled', 'frizzled', 'fuchsia', 'funny', 'gabby', 'galaxy', 'galvanize', 'gazebo', 'giaour', 'gizmo', 'glowworm', 'glyph', 'gnarly', 'gnostic', 'gossip', 'grogginess', 'haiku', 'haphazard', 'hyphen', 'iatrogenic', 'icebox', 'injury', 'ivory', 'ivy', 'jackpot', 'jaundice', 'jawbreaker', 'jaywalk', 'jazziest', 'jazzy', 'jelly', 'jigsaw', 'jinx', 'jiujitsu', 'jockey', 'jogging', 'joking', 'jovial', 'joyful', 'juicy', 'jukebox', 'jumbo', 'kayak', 'kazoo', 'keyhole', 'khaki', 'kilobyte', 'kiosk', 'kitsch', 'kiwifruit', 'klutz', 'knapsack', 'larynx', 'lengths', 'lucky', 'luxury', 'lymph', 'marquis', 'matrix', 'megahertz', 'microwave', 'mnemonic', 'mystify', 'naphtha', 'nightclub', 'nowadays', 'numbskull', 'nymph', 'onyx', 'ovary', 'oxidize', 'oxygen', 'pajama', 'peekaboo', 'phlegm', 'pixel', 'pizazz', 'pneumonia', 'polka', 'pshaw', 'psyche', 'puppy', 'puzzling', 'quartz', 'queue', 'quips', 'quixotic', 'quiz', 'quizzes', 'quorum', 'razzmatazz', 'rhubarb', 'rhythm', 'rickshaw', 'schnapps', 'scratch', 'shiv', 'snazzy', 'sphinx', 'spritz', 'squawk', 'staff', 'strength', 'strengths', 'stretch', 'stronghold', 'stymied', 'subway', 'swivel', 'syndrome', 'thriftless', 'thumbscrew', 'topaz', 'transcript', 'transgress', 'transplant', 'triphthong', 'twelfth', 'twelfths', 'unknown', 'unworthy', 'unzip', 'uptown', 'vaporize', 'vixen', 'vodka', 'voodoo', 'vortex', 'voyeurism', 'walkway', 'waltz', 'wave', 'wavy', 'waxy', 'wellspring', 'wheezy', 'whiskey', 'whizzing', 'whomever', 'wimpy', 'witchcraft', 'wizard', 'woozy', 'wristwatch', 'wyvern', 'xylophone', 'yachtsman', 'yippee', 'yoked', 'youthful', 'yummy', 'zephyr', 'zigzag', 'zigzagging', 'zilch', 'zipper', 'zodiac', 'zombie']
+# Stages Of Hangman
+Hangman_Stages = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''', '''
+  +---+
+  |   |
+      |
+  O   |
+ /|\  |
+ / \  |
+=========''']
+
+def hangman():
+    life = 6
+    guessword = random.choice(words)
+    guessword_letters = set(guessword)
+    used_letters = set()
+    answer = "_"*len(guessword)
+    print(answer)
+    while guessword != answer and life > 0:
+        user_letter = input("Guess the letter: ").lower()
+        if user_letter in used_letters:
+            print("Letter already used. Try another")
+        elif user_letter in guessword_letters:
+            print("Right guess.")
+            for i in range(len(guessword)):
+                if user_letter == guessword[i]:
+                    answer = answer[:i] + user_letter + answer[i+1:]
+        elif user_letter.isalpha():
+            life -= 1
+            print(f"Wrong guess, {life} lives left.")
+        else:
+            print("Invalid guess.")
+        if user_letter not in used_letters:
+            used_letters.add(user_letter)
+        print(answer)
+        print(Hangman_Stages[6-life])
+    if guessword != answer:
+        print(f"You died. The word was '{guessword}'")
+    else:
+        print(Hangman_Stages[6])
+        print("Congratulations! You Won.")
+        
+hangman()
+
